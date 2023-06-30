a64aba4a1ac15f9b30295d31f89a981292d22b05
Mst Sharmina Iasmin <92200149+sharmina-lina@users.noreply.github.com>
2023-06-30 22:53:11 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Metronome from "./Metronome";
+import "./index.css";
+//import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<Metronome />, document.getElementById("root"));
+//registerServiceWorker();
